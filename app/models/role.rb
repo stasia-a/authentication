9f72286402a1409188edc9d55b552f1067a47e3d
Anastasia Charushina <stasia-a@tut.by>
2012-09-28 12:00:17 +0300

    Added some validations

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,5 @@ class Role < ActiveRecord::Base
   attr_accessible :role_of_user
 
   belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :login
 
+  before_save { |user| user.email = email.downcase }
+
   has_one :role, :dependent => :destroy
   accepts_nested_attributes_for :role
 
-  validates :email,:login, presence: true
+  validates :login, presence: true, uniqueness: true
+  validates :email, presence: true, email: true, uniqueness: true
 
 end
